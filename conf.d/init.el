3d5c2509c020c8e1140701635a11e511aefc4dbd
孙昊航 <nicewise@qq.com>
2023-04-04 14:25:28 +0800

    emacs org-ref

--- conf.d/init.el
+++ conf.d/init.el
@@ -2,9 +2,26 @@
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (global-display-line-numbers-mode t)
+(global-auto-revert-mode t)
+(setq auto-revert-interval 5)
+(setq org-directory "~/notes")
+(setq org-defaul-notes-file "~/notes/quicknote.org")
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+;; helm
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/notes/quicknote.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "~/notes/dairy.org")
+         "* %?\nEntered on %U\n  %i\n  %a")
+	("n" "note" entry (file "~/notes/quicknote.org")
+         "* %? :NOTE:\n%U\n%a\n")))
 (setq package-archives '(("gnu"   . "http://mirror.nju.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirror.nju.edu.cn/elpa/melpa/")))
 (package-initialize) ;; You might already have this line
@@ -12,6 +29,58 @@
 (setq TeX-parse-self t)
 (setq-default TeX-engine 'xetex)
 (setq-default TeX-PDF-mode t)
+(setq bibtex-completion-bibliography '("~/notes/ref.bib")
+      bibtex-completion-library-path '("~/Research/")
+      ;bibtex-completion-pdf-field "File"
+      bibtex-completion-find-additional-pdfs t
+      bibtex-completion-pdf-extension '(".pdf" ".djvu" ".ps")
+      bibtex-completion-pdf-open-function
+      (lambda (fpath)
+	(call-process "zathura" nil 0 nil fpath))
+      bibtex-completion-pdf-symbol "⌘"
+      bibtex-completion-notes-symbol "✎"
+      bibtex-completion-notes-path "~/notes/reading.org"
+      bibtex-completion-notes-template-one-file
+      (concat
+       "** ${title}\n"
+       "    :PROPERTIES:\n"
+       "      :Custom_ID: ${=key=}\n"
+       "      :AUTHOR: ${author-or-editor}\n"
+       "      :JOURNAL: ${journal}\n"
+       "      :YEAR: ${year}\n"
+       "      :DOI: ${doi}\n"
+       "      :URL: ${url}\n"
+       "    :END:\n\n"
+       "[[cite:&${=key=}]]\n"
+       )
+      bibtex-completion-additional-search-fields '(keywords)
+      bibtex-completion-display-formats
+      '((article       . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${journal:40}")
+	(inbook        . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} Chapter ${chapter:32}")
+	(incollection  . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
+	(inproceedings . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
+	(t             . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*}"))
+      )
+(require 'bibtex)
+
+(setq bibtex-autokey-year-length 4
+      bibtex-autokey-name-year-separator "-"
+      bibtex-autokey-year-title-separator "-"
+      bibtex-autokey-titleword-separator "-"
+      bibtex-autokey-titlewords 2
+      bibtex-autokey-titlewords-stretch 1
+      bibtex-autokey-titleword-length 5)
+
+(define-key bibtex-mode-map (kbd "C-c ]") 'org-ref-bibtex-hydra/body)
+(require 'org-ref)
+(require 'org-ref-helm)
+(define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link-hydra/body)
+
+(setq org-file-apps
+      (quote
+       ((auto-mode . emacs)
+	("\\.pdf\\'" . "evince %s"))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -98,6 +167,8 @@
      (output-dvi "xdvi")
      (output-pdf "Zathura")
      (output-html "xdg-open")))
+ '(org-agenda-files
+   '("~/notes/reading.org" "/home/sunhh/notes/notes.org" "/home/sunhh/notes/sch.org" "/home/sunhh/notes/quicknote.org"))
  '(org-latex-classes
    '(("article" "\\documentclass[UTF8,a4paper]{article}"
       ("\\section{%s}" . "\\section*{%s}")
@@ -120,8 +191,34 @@
  '(org-latex-compiler "xelatex")
  '(org-latex-default-packages-alist nil)
  '(org-latex-hyperref-template nil)
+ '(org-preview-latex-default-process 'dvisvgm)
+ '(org-preview-latex-process-alist
+   '((dvipng :programs
+	     ("latex" "dvipng")
+	     :description "dvi > png" :message "you need to install the programs: latex and dvipng." :image-input-type "dvi" :image-output-type "png" :image-size-adjust
+	     (1.0 . 1.0)
+	     :latex-compiler
+	     ("latex -interaction nonstopmode -output-directory %o %f")
+	     :image-converter
+	     ("dvipng -D %D -T tight -bg Transparent -o %O %f"))
+     (dvisvgm :programs
+	      ("latex" "dvisvgm")
+	      :description "dvi > svg" :message "you need to install the programs: latex and dvisvgm." :image-input-type "dvi" :image-output-type "svg" :image-size-adjust
+	      (1.7 . 1.5)
+	      :latex-compiler
+	      ("latex -interaction nonstopmode -output-directory %o %f")
+	      :image-converter
+	      ("dvisvgm %f -n -b min -c %S -o %O"))
+     (imagemagick :programs
+		  ("latex" "convert")
+		  :description "pdf > png" :message "you need to install the programs: latex and imagemagick." :image-input-type "pdf" :image-output-type "png" :image-size-adjust
+		  (1.0 . 1.0)
+		  :latex-compiler
+		  ("pdflatex -interaction nonstopmode -output-directory %o %f")
+		  :image-converter
+		  ("convert -density %D -trim -antialias %f -quality 100 %O"))))
  '(package-selected-packages
-   '(jupyter sudoku minesweeper 2048-game org-modern preview-dvisvgm markdown-mode magit julia-mode cdlatex org-ref org auctex yasnippet-classic-snippets yasnippet))
+   '(helm-bibtex julia-vterm jupyter sudoku minesweeper 2048-game org-modern preview-dvisvgm markdown-mode magit julia-mode cdlatex org-ref org auctex yasnippet-classic-snippets yasnippet))
  '(preview-image-type 'dvisvgm))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
