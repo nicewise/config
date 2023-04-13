b471576806d0eddcc98f4d476fcea5119200e6e3
孙昊航 <nicewise@qq.com>
2023-04-13 21:58:02 +0800

    emacs config 初步

--- conf.d/init.el
+++ conf.d/init.el
@@ -1,20 +1,43 @@
+;; emacs
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (global-display-line-numbers-mode t)
 (global-auto-revert-mode t)
 (setq auto-revert-interval 5)
-(setq org-directory "~/notes")
-(setq org-defaul-notes-file "~/notes/quicknote.org")
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
+
+;; packages
+(setq package-archives '(("gnu"   . "http://mirror.nju.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirror.nju.edu.cn/elpa/melpa/")))
+(package-initialize) ;; You might already have this line
+(defvar my-packages '(magit
+		      org-ref
+		      julia-mode
+		      helm-bibtex
+		      cdlatex
+		      auctex
+                      rainbow-delimiters))
+
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
 ;; helm
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (helm-mode 1)
+(global-set-key (kbd "C-c h") #'helm-bibtex)
 
+;; latex-mode
+(setq-default TeX-engine 'xetex)
+
+;; org-mode
+(setq org-directory "~/notes")
+(setq org-defaul-notes-file "~/notes/quicknote.org")
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/notes/quicknote.org" "Tasks")
          "* TODO %?\n  %i\n  %a")
@@ -22,13 +45,12 @@
          "* %?\nEntered on %U\n  %i\n  %a")
 	("n" "note" entry (file "~/notes/quicknote.org")
          "* %? :NOTE:\n%U\n%a\n")))
-(setq package-archives '(("gnu"   . "http://mirror.nju.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirror.nju.edu.cn/elpa/melpa/")))
-(package-initialize) ;; You might already have this line
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq-default TeX-engine 'xetex)
-(setq-default TeX-PDF-mode t)
+(setq org-file-apps
+      (quote
+       ((auto-mode . emacs)
+	("\\.pdf\\'" . "zathura %s"))))
+
+;; org-ref
 (setq bibtex-completion-bibliography '("~/notes/ref.bib")
       bibtex-completion-library-path '("~/Research/")
       ;bibtex-completion-pdf-field "File"
@@ -59,10 +81,8 @@
 	(inbook        . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} Chapter ${chapter:32}")
 	(incollection  . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
 	(inproceedings . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
-	(t             . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*}"))
-      )
+	(t             . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*}")))
 (require 'bibtex)
-
 (setq bibtex-autokey-year-length 4
       bibtex-autokey-name-year-separator "-"
       bibtex-autokey-year-title-separator "-"
@@ -70,161 +90,78 @@
       bibtex-autokey-titlewords 2
       bibtex-autokey-titlewords-stretch 1
       bibtex-autokey-titleword-length 5)
-
 (define-key bibtex-mode-map (kbd "C-c ]") 'org-ref-bibtex-hydra/body)
 (require 'org-ref)
 (require 'org-ref-helm)
 (define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link-hydra/body)
 
-(setq org-file-apps
-      (quote
-       ((auto-mode . emacs)
-	("\\.pdf\\'" . "evince %s"))))
+;; org-latex
+(setq org-preview-latex-default-process 'dvisvgm)
+(defun my/text-scale-adjust-latex-previews ()
+  "Adjust the size of latex preview fragments when changing the
+buffer's text scale."
+  (pcase major-mode
+    ('latex-mode
+     (dolist (ov (overlays-in (point-min) (point-max)))
+       (if (eq (overlay-get ov 'category)
+               'preview-overlay)
+           (my/text-scale--resize-fragment ov))))
+    ('org-mode
+     (dolist (ov (overlays-in (point-min) (point-max)))
+       (if (eq (overlay-get ov 'org-overlay-type)
+               'org-latex-overlay)
+           (my/text-scale--resize-fragment ov))))))
+(defun my/text-scale--resize-fragment (ov)
+  (overlay-put
+   ov 'display
+   (cons 'image
+         (plist-put
+          (cdr (overlay-get ov 'display))
+          :scale (+ 1.0 (* 0.25 text-scale-mode-amount))))))
+(add-hook 'text-scale-mode-hook #'my/text-scale-adjust-latex-previews)
+;; org-latex-export
+(setq org-latex-default-packages-alist nil)
+(setq org-latex-packages-alist
+      '(
+	("zihao=-4,heading=true,punct=plain" "ctex")
+	("bookmarks=true" "hyperref")
+	("" "amssymb")
+	("" "amsmath")
+	("" "xcolor")
+	("" "graphicx")
+	("" "bm")
+	("left=1in,right=1in,top=1in,bottom=1in" "geometry")
+	("" "fancyhdr")
+	"\\pagestyle{fancy}"
+	"\\fancyhead[R]{}"
+	"\\fancyfoot[L]{孙昊航}"
+	"\\fancyfoot[C]{}"
+	"\\fancyfoot[R]{\\thepage}"
+	"\\renewcommand{\\headrulewidth}{0pt}"
+	"\\renewcommand{\\footrulewidth}{0.4pt}"
+	("" "tikz")
+	("" "pgfplotstable")
+	"\\pgfplotsset{compat=1.16}"
+	)
+      )
+(setq org-latex-hyperref-template nil)
+(setq org-latex-compiler "xelatex")
 
+;; og-babel
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)
+   (julia . t)
+   (emacs-lisp . t)))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-command-list
-   '(("TeX" "%(PDF)%(tex) %(file-line-error) %`%(extraopts) %S%(PDFout)%(mode)%' %(output-dir) %t" TeX-run-TeX nil
-      (plain-tex-mode texinfo-mode ams-tex-mode)
-      :help "Run plain TeX")
-     ("LaTeX" "%`%l%(mode)%' %T" TeX-run-TeX nil
-      (latex-mode doctex-mode)
-      :help "Run LaTeX")
-     ("Makeinfo" "makeinfo %(extraopts) %(o-dir) %t" TeX-run-compile nil
-      (texinfo-mode)
-      :help "Run Makeinfo with Info output")
-     ("Makeinfo HTML" "makeinfo %(extraopts) %(o-dir) --html %t" TeX-run-compile nil
-      (texinfo-mode)
-      :help "Run Makeinfo with HTML output")
-     ("AmSTeX" "amstex %(PDFout) %`%(extraopts) %S%(mode)%' %(output-dir) %t" TeX-run-TeX nil
-      (ams-tex-mode)
-      :help "Run AMSTeX")
-     ("ConTeXt" "%(cntxcom) --once --texutil %(extraopts) %(execopts)%t" TeX-run-TeX nil
-      (context-mode)
-      :help "Run ConTeXt once")
-     ("ConTeXt Full" "%(cntxcom) %(extraopts) %(execopts)%t" TeX-run-TeX nil
-      (context-mode)
-      :help "Run ConTeXt until completion")
-     ("BibTeX" "bibtex %(O?aux)" TeX-run-BibTeX nil
-      (plain-tex-mode latex-mode doctex-mode context-mode texinfo-mode ams-tex-mode)
-      :help "Run BibTeX")
-     ("Biber" "biber %(output-dir) %s" TeX-run-Biber t
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run Biber")
-     ("Texindex" "texindex %s.??" TeX-run-command nil
-      (texinfo-mode)
-      :help "Run Texindex")
-     ("Texi2dvi" "%(PDF)texi2dvi %t" TeX-run-command nil
-      (texinfo-mode)
-      :help "Run Texi2dvi or Texi2pdf")
-     ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
-     ("Print" "%p" TeX-run-command t t :help "Print the file")
-     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
-     ("File" "%(o?)dvips %d -o %f " TeX-run-dvips t
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Generate PostScript file")
-     ("Dvips" "%(o?)dvips %d -o %f " TeX-run-dvips nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Convert DVI file to PostScript")
-     ("Dvipdfmx" "dvipdfmx -o %(O?pdf) %d" TeX-run-dvipdfmx nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Convert DVI file to PDF with dvipdfmx")
-     ("Ps2pdf" "ps2pdf %f %(O?pdf)" TeX-run-ps2pdf nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Convert PostScript file to PDF")
-     ("Glossaries" "makeglossaries %(d-dir) %s" TeX-run-command nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run makeglossaries to create glossary file")
-     ("Index" "makeindex %(O?idx)" TeX-run-index nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run makeindex to create index file")
-     ("upMendex" "upmendex %(O?idx)" TeX-run-index t
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run upmendex to create index file")
-     ("Xindy" "texindy %s" TeX-run-command nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run xindy to create index file")
-     ("Check" "lacheck %s" TeX-run-compile nil
-      (latex-mode)
-      :help "Check LaTeX file for correctness")
-     ("ChkTeX" "chktex -v6 %s" TeX-run-compile nil
-      (latex-mode)
-      :help "Check LaTeX file for common mistakes")
-     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
-     ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
-     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
-     ("Other" "" TeX-run-command t t :help "Run an arbitrary command")))
- '(TeX-save-query nil)
- '(TeX-source-correlate-mode t)
- '(TeX-view-program-selection
-   '(((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "Zathura")
-     (output-html "xdg-open")))
- '(org-agenda-files
-   '("~/notes/reading.org" "/home/sunhh/notes/notes.org" "/home/sunhh/notes/sch.org" "/home/sunhh/notes/quicknote.org"))
- '(org-latex-classes
-   '(("article" "\\documentclass[UTF8,a4paper]{article}"
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-      ("\\paragraph{%s}" . "\\paragraph*{%s}")
-      ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-     ("report" "\\documentclass[11pt]{report}"
-      ("\\part{%s}" . "\\part*{%s}")
-      ("\\chapter{%s}" . "\\chapter*{%s}")
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
-     ("book" "\\documentclass[11pt]{book}"
-      ("\\part{%s}" . "\\part*{%s}")
-      ("\\chapter{%s}" . "\\chapter*{%s}")
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
- '(org-latex-compiler "xelatex")
- '(org-latex-default-packages-alist nil)
- '(org-latex-hyperref-template nil)
- '(org-preview-latex-default-process 'dvisvgm)
- '(org-preview-latex-process-alist
-   '((dvipng :programs
-	     ("latex" "dvipng")
-	     :description "dvi > png" :message "you need to install the programs: latex and dvipng." :image-input-type "dvi" :image-output-type "png" :image-size-adjust
-	     (1.0 . 1.0)
-	     :latex-compiler
-	     ("latex -interaction nonstopmode -output-directory %o %f")
-	     :image-converter
-	     ("dvipng -D %D -T tight -bg Transparent -o %O %f"))
-     (dvisvgm :programs
-	      ("latex" "dvisvgm")
-	      :description "dvi > svg" :message "you need to install the programs: latex and dvisvgm." :image-input-type "dvi" :image-output-type "svg" :image-size-adjust
-	      (1.7 . 1.5)
-	      :latex-compiler
-	      ("latex -interaction nonstopmode -output-directory %o %f")
-	      :image-converter
-	      ("dvisvgm %f -n -b min -c %S -o %O"))
-     (imagemagick :programs
-		  ("latex" "convert")
-		  :description "pdf > png" :message "you need to install the programs: latex and imagemagick." :image-input-type "pdf" :image-output-type "png" :image-size-adjust
-		  (1.0 . 1.0)
-		  :latex-compiler
-		  ("pdflatex -interaction nonstopmode -output-directory %o %f")
-		  :image-converter
-		  ("convert -density %D -trim -antialias %f -quality 100 %O"))))
- '(package-selected-packages
-   '(helm-bibtex julia-vterm jupyter sudoku minesweeper 2048-game org-modern preview-dvisvgm markdown-mode magit julia-mode cdlatex org-ref org auctex yasnippet-classic-snippets yasnippet))
- '(preview-image-type 'dvisvgm))
+ '(org-agenda-files '("~/notes/reading.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'set-goal-column 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
