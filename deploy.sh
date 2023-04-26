c78ded91c32764806a77a14a65e9d1fa5a44f124
孙昊航 <nicewise@qq.com>
2023-04-26 21:32:56 +0800

    rockstudio

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 gitdir="$HOME/config"
 # relative path from ~, i.e. the home directory
-id=" "
-#CAUTION!! FILL THIS BLANK!!
 cd ~
 ln -sf $gitdir/conf.d/xinitrc .xinitrc
 ln -sf $gitdir/conf.d/xserverrc .xserverrc
@@ -9,12 +7,13 @@ ln -sf $gitdir/conf.d/conkyrc .conkyrc
 ln -sf $gitdir/conf.d/xprofile .xprofile
 ln -sf $gitdir/conf.d/Xresources .Xresources
 ln -sf $gitdir/conf.d/vimrc .vimrc
+ln -sf $gitdir/conf.d/bashrc .bashrc
 
 if [ ! -d .config/i3 ];then
 	mkdir -p .config/i3
 fi
 #ln -sf $gitdir/conf.d/i3config_laptop .config/i3/config
-#ln -sf $gitdir/conf.d/i3config_workstation .config/i3/config
+ln -sf $gitdir/conf.d/i3config_workstation .config/i3/config
 #choose one according to tour situation and uncomment it.
 
 if [ ! -d .emacs.d ];then
@@ -25,10 +24,6 @@ ln -sf $gitdir/conf.d/init.el .emacs.d/init.el
 #sudo ln -sf $gitdir/conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 #uncomment this if necessary
 
-if [ ! -d $gitdir/$id ];then
-	mkdir $gitdir/$id
-else
-	echo "$gitdir/$id already exists!"
-fi
-cp $gitdir/conf.d/bashrc $gitdir/$id/bashrc
-ln -sf $gitdir/$id/bashrc .bashrc
+
+sudo cp $gitdir/conf.d/config.json /etc/v2ray/
+#uncomment this if necessary
