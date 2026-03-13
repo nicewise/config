287a396c0b1e1663dae05fe385d1b443930b472b
Sun Haohang(工位版) <nicewise@qq.com>
2026-03-14 05:19:47 +0800

    fix: 添加 rofi 配置，修复 swaylock 部署路径

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # config
++ set wallpaper(s)
+```bash
+  ln -sf /path/to/real/picture $HOME/config/conf.d/background
+  ln -sf /path/to/real/picture $HOME/config/conf.d/wallpaper1
+  ln -sf /path/to/real/picture $HOME/config/conf.d/wallpaper2
+```
 + bash deploy.sh
 + push update
--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,47 @@ gitdir="$HOME/config"
 
 cd ~
 
-# 普通文件
-for f in xinitrc xserverrc conkyrc xprofile Xresources vimrc bashrc zshrc; do
+# X11
+for f in xinitrc xserverrc xprofile Xresources; do
   ln -sf "$gitdir/conf.d/$f" ".$f"
 done
+
+# 系统级 (Xorg)
+sudo ln -sf "$gitdir/conf.d/30-touchpad.conf" /etc/X11/xorg.conf.d/30-touchpad.conf
+sudo ln -sf "$gitdir/conf.d/10-monitor.conf" /etc/X11/xorg.conf.d/10-monitor.conf
 
 # i3
 mkdir -p .config/i3
 ln -sf "$gitdir/conf.d/i3config" .config/i3/config
+ln -sf "$gitdir/conf.d/conkyrc" ".conkyrc"
+
+# sway
+mkdir -p .config/sway
+ln -sf "$gitdir/conf.d/swayconfig" .config/sway/config
+mkdir -p .config/swaylock
+ln -sf "$gitdir/conf.d/swaylock" .config/swaylock/config
+
+# rofi
+mkdir -p .config/rofi
+ln -sf "$gitdir/conf.d/rofi/config.rasi" .config/rofi/config.rasi
+
+# waybar
+mkdir -p .config/waybar
+ln -sf "$gitdir/conf.d/waybar/config" .config/waybar/config
+ln -sf "$gitdir/conf.d/waybar/style.css" .config/waybar/style.css
+
+# zsh
+for f in zprofile zshrc; do
+  ln -sf "$gitdir/conf.d/$f" ".$f"
+done
+
+# vim
+ln -sf "$gitdir/conf.d/vimrc" ".vimrc"
 
 # emacs
 mkdir -p .emacs.d
 ln -sf "$gitdir/conf.d/init.el" .emacs.d/init.el
+
 # alacritty
 mkdir -p .config/alacritty
 ln -sf "$gitdir/conf.d/alacritty.toml" .config/alacritty/alacritty.toml
@@ -24,23 +53,3 @@ ln -sf "$gitdir/conf.d/alacritty.toml" .config/alacritty/alacritty.toml
 # paru
 mkdir -p .config/paru
 ln -sf "$gitdir/conf.d/paru.conf" .config/paru/paru.conf
-
-# 系统级
-sudo ln -sf "$gitdir/conf.d/sunhhenv.sh" /etc/profile.d/
-sudo ln -sf "$gitdir/conf.d/30-touchpad.conf" /etc/X11/xorg.conf.d/30-touchpad.conf
-sudo ln -sf "$gitdir/conf.d/10-monitor.conf" /etc/X11/xorg.conf.d/10-monitor.conf
-
-# sway
-mkdir -p .config/sway
-ln -sf "$gitdir/conf.d/swayconfig" .config/sway/config
-mkdir -p .config/swaylock
-ln -sf "$gitdir/conf.d/swaylockconfig" .config/swaylock/config
-ln -sf "$gitdir/conf.d/swayshot.sh" "$HOME/.local/bin/swayshot.sh"
-
-# waybar
-mkdir -p .config/waybar
-ln -sf "$gitdir/conf.d/waybar/config" .config/waybar/config
-ln -sf "$gitdir/conf.d/waybar/style.css" .config/waybar/style.css
-
-# sway env
-ln -sf "$gitdir/conf.d/swayenv" .config/sway/env
